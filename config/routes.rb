823bc2a440d4381c97c50313be50b165d046d02e
thsvr <vr.ths.zd@gmail.com>
2019-11-05 19:24:15 -0300

    deleted wrong content

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'welcome/home', to: 'welcome#home'
   get 'welcome/about', to: 'welcome#about'
-  get 'alpha-blog/home', to: 'alpha-blog#home'
-  get 'alpha-blog/about', to: 'alpha-blog#home'
 end
