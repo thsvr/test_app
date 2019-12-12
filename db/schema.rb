3fc4de0794a8b8acf3bbca83b5804db4106f920f
thsvr <vr.ths.zd@gmail.com>
2019-12-12 17:12:57 -0300

    adding has_many to test ASSOCIATIONS - NOT WORKING ALREADY

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_184721) do
+ActiveRecord::Schema.define(version: 2019_12_12_194253) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2019_12_12_184721) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "users"
 end
